f5723302593231a43f08912d199b59f8cb2de349
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-10-11 20:33:28 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'signup';
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.initForm();
+  }
+  initForm() {
+    this.loginForm = this.formBuilder.group({
+      // tslint:disable-next-line:max-line-length
+      email: ['', Validators.compose([Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)])],
+      password: ['', Validators.compose([Validators.required])]
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      alert('success login');
+    }
+  }
 }
